0deeb7b6d4687cf5dacc06179d4d017dc71ef082
Edgar-Garcia-1063 <164534098+Edgar-Garcia-1063@users.noreply.github.com>
2025-02-07 16:48:14 +0000

    mi primera vez

---
+++ funciones1.dart
@@ -0,0 +1,15 @@
+void Saludar() {
+  print("hola mundo");
+}
+void escribenombre() {
+}
+
+void main() {
+  //Edgar Axel Garcia Aguirre
+  //llamar a la funcion saludar
+  print("Edgar Axel Garcia Aguirre: 22308051281063");
+  print("llamando a la funcion saludar");
+  Saludar();
+  print("llamando a la funcion escribenombre");
+  escribenombre()
+}
